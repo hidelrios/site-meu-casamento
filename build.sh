e3ed75b97f43d2d4ae4c78429a2d2c0c3b3e41c9
hidelrios <hidels.rios@gmail.com>
2025-10-06 12:10:54 -0300

    feat: ajustes

--- build.sh
+++ build.sh
@@ -6,3 +6,11 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+# Cria superusuário automaticamente
+if [ "$CREATE_SUPERUSER" = "True" ]; then
+  python manage.py createsuperuser \
+    --email "$DJANGO_SUPERUSER_EMAIL" \
+    --username "$DJANGO_SUPERUSER_USERNAME" \
+    --no-input
+fi
